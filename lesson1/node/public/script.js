93fa943ca968fe9569698e31b45b6ee91c304dd7
ashmidt <Joujvemkiefjoft3>
2021-03-26 00:32:32 +0300

    lesson7

--- lesson1/node/public/script.js
+++ lesson1/node/public/script.js
@@ -1,6 +1,6 @@
-const API_URL = "goods.json";
+const API_URL = 'http://localhost:3003/';
 
-Vue.component("goods-item", {
+Vue.component('goods-item', {
   // Создание нового компонента
   template: `<div :data-id="id" class="goods-item">
       <div class="good-item-img-placeholder"></div>
@@ -8,17 +8,16 @@ Vue.component("goods-item", {
       <p>{{ price }}</p>
       <button class="good-item-button" @click="addToCart">Купить</button>
     </div>`,
-  props: ["title", "price", "id"], // задаем параметры компонента
+  props: ['title', 'price', 'id'], // задаем параметры компонента
   methods: {
     addToCart() {
       this.$emit('addToCart', this.id);
-    }
-  }
+    },
+  },
 });
 
 Vue.component('goods-list', {
   template: `<div class="goods-list">
-                <div v-if="!goods || goods.length === 0">Нет товаров</div>
                 <goods-item
                   v-for="good in goods"
                   :title="good.title"
@@ -33,11 +32,11 @@ Vue.component('goods-list', {
   methods: {
     addToCartHandler(id) {
       this.$emit('addToCart', id);
-    }
-  }
-})
+    },
+  },
+});
 
-Vue.component("cart-item", {
+Vue.component('cart-item', {
   template: `<tr data-id="id">
             <td>{{ title }}</td>
             <td>{{ price }}</td>
@@ -50,7 +49,7 @@ Vue.component("cart-item", {
             </td>
        </tr>`,
 
-  props: ["title", "price", "id", "quantity"], // задаем параметры компонента
+  props: ['title', 'price', 'id', 'quantity'], // задаем параметры компонента
 
   computed: {
     totalPrice() {
@@ -60,20 +59,20 @@ Vue.component("cart-item", {
 
   methods: {
     increaseHandler() {
-      this.$emit("increaseQuantity", this.id);
+      this.$emit('increaseQuantity', this.id);
     },
 
     decreaseHandler() {
-      this.$emit("decreaseQuantity", this.id);
+      this.$emit('decreaseQuantity', this.id);
     },
 
     removeHandler() {
-      this.$emit("remove", this.id);
+      this.$emit('remove', this.id);
     },
   },
 });
 
-Vue.component("cart", {
+Vue.component('cart', {
   template: `<div class="cartWrapper">
                 <button class="cart-button" @click="openCartHandler" type="button">Корзина ({{totalCount}}|{{totalPrice}}р)</button>
                 <div class="cartPopup" v-if="isVisibleCart">
@@ -112,12 +111,15 @@ Vue.component("cart", {
 
   computed: {
     totalCount() {
-      return this.cartItems.reduce((acc, {quantity }) => acc + quantity, 0);
+      return this.cartItems.reduce((acc, { quantity }) => acc + quantity, 0);
     },
 
     totalPrice() {
-      return this.cartItems.reduce((acc, {price, quantity }) => acc + price * quantity, 0);
-    }
+      return this.cartItems.reduce(
+        (acc, { price, quantity }) => acc + price * quantity,
+        0,
+      );
+    },
   },
 
   methods: {
@@ -126,32 +128,45 @@ Vue.component("cart", {
     },
 
     increaseHandler(id) {
-      this.$emit("increaseQuantity", id);
+      this.$emit('increaseQuantity', id);
     },
 
     decreaseHandler(id) {
-      this.$emit("decreaseQuantity", id);
+      this.$emit('decreaseQuantity', id);
     },
 
     removeHandler(id) {
-      this.$emit("remove", id);
+      this.$emit('remove', id);
     },
   },
-
 });
 
-Vue.component("search", {
+Vue.component('search', {
   template: `<input id="search" @input="searchHandler" placeholder="Поиск..." />`,
 
   methods: {
     searchHandler(e) {
-      this.$emit("valueChange", e);
+      this.$emit('valueChange', e);
     },
   },
 });
 
+Vue.component('loader', {
+  template: `
+    <div>
+      <div v-if="isLoading">Загрузка</div>
+      <div v-if="!isLoading && !isSuccessFetch">Упс, что-то не так. Уже чиним</div>
+    </div>
+`,
+
+  props: {
+    isLoading: Boolean,
+    isSuccessFetch: Boolean,
+  },
+});
+
 const vue = new Vue({
-  el: "#app",
+  el: '#app',
   template: `<div>
                 <header class="header">
                     <search @valueChange="searchHandler" />
@@ -163,16 +178,37 @@ const vue = new Vue({
                     />
                 </header>
                 <main>
-                    <goods-list :goods="filtredGoods" @addToCart="addToCartHandler"/>
+                    <loader :isLoading='isLoading' :isSuccessFetch='isSuccessFetch'/>
+                    <goods-list v-if="isSuccessFetch" :goods="filtredGoods" @addToCart="addToCartHandler"/>
                 </main>
             </div>`,
   data: {
     cart: [],
     goods: [],
     filtredGoods: [],
-    search: "",
+    search: '',
+    isLoading: true,
+    isSuccessFetch: false,
   },
   methods: {
+    updateCart() {
+      // каждое изменение корзины закидываем на сервер и забираем копию с сервера.
+      // Таким образом точкой истины является файл на сервере
+      fetch(`${API_URL}cart`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.cart),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.cart = data;
+        });
+    },
+
     addToCartHandler(id) {
       const good = this.cart.find((item) => item.id == id);
 
@@ -182,53 +218,61 @@ const vue = new Vue({
         const initialGood = this.goods.find((item) => item.id == id);
         this.cart.push({ ...initialGood, quantity: 1 });
       }
+
+      this.updateCart();
     },
 
     increaseQuantityInCartHandler(id) {
-      const good = this.cart.find((item) => item.id == id)
+      const good = this.cart.find((item) => item.id == id);
 
       if (good) good.quantity++;
+
+      this.updateCart();
     },
 
     decreaseQuantityInCartHandler(id) {
-
-      const good = this.cart.find((item) => item.id == id)
+      const good = this.cart.find((item) => item.id == id);
 
       if (good && good.quantity >= 1) good.quantity--;
 
       if (good && good.quantity <= 0) {
-        this.removeFromCartHandler(id)
+        this.removeFromCartHandler(id);
       }
+
+      this.updateCart();
     },
 
     removeFromCartHandler(id) {
       const goodIndex = this.cart.findIndex((item) => item.id == id);
 
       this.cart.splice(goodIndex, 1);
+
+      this.updateCart();
     },
 
     searchHandler(e) {
       const {
         target: { value },
       } = e;
-      if (value === "") {
+      if (value === '') {
         this.filtredGoods = this.goods;
       }
-      const regexp = new RegExp(value, "gi");
+      const regexp = new RegExp(value, 'gi');
       this.filtredGoods = this.goods.filter((good) => regexp.test(good.title));
     },
 
     fetchPromise() {
-      // пока не писали свой сервер - используем статику
-      return new Promise((resolve) => {
-        resolve([
-          { id: 1, title: "Coat", price: 450 },
-          { id: 2, title: "Shirt", price: 150 },
-          { id: 3, title: "Socks", price: 50 },
-          { id: 4, title: "Jacket", price: 350 },
-          { id: 5, title: "Shoes", price: 250 },
-        ]);
-      });
+      return fetch(`${API_URL}data`)
+        .then((response) => {
+          this.isLoading = false;
+          this.isSuccessFetch = true;
+          return response.json();
+        })
+        .catch((err) => {
+          this.isLoading = false;
+          this.isSuccessFetch = false;
+          console.log('Fetch sucs. Smth went wrong');
+        });
     },
   },
   mounted() {
@@ -240,5 +284,30 @@ const vue = new Vue({
       .catch((err) => {
         console.log(err);
       });
+
+    fetch(`${API_URL}cart`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.cart = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 });
+
+// запрос для создания нового товара
+const addGood = () => {
+  fetch(`${API_URL}data`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: 'NEW Calc!',
+      price: '10000',
+    }),
+  });
+};
